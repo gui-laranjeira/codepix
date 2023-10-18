892d6503c34ae56476d74c975a5800f71a53a47c
root <root@DESKTOP-5KLG8OH>
2023-10-18 15:44:34 -0300

    feat: setting up models to gorm mapping

--- codepix/domain/model/transaction.go
+++ codepix/domain/model/transaction.go
@@ -28,11 +28,13 @@ type Transactions struct {
 type Transaction struct {
 	Base              `valid:"required"`
 	AccountFrom       *Account `valid:"-"`
-	Amount            float64  `json:"amount" valid:"notnull"`
+	AccountFromID     string   `gorm:"column:account_from_id;type:uuid;" valid:"notnull"`
+	Amount            float64  `json:"amount" gorm:"type:float" valid:"notnull"`
 	PixKeyTo          *PixKey  `valid:"-"`
-	Status            string   `json:"status" valid:"notnull"`
-	Description       string   `json:"description" valid:"notnull"`
-	CancelDescription string   `json:"cancel_description" valid:"notnull"`
+	PixKeyIdTo        string   `gorm:"column:pix_key_id_to;type:uuid;" valid:"notnull"`
+	Status            string   `json:"status" gorm:"type:varchar(20)" valid:"notnull"`
+	Description       string   `json:"description" gorm:"type:varchar(255)" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" gorm:"type:varchar(255)" valid:"notnull"`
 }
 
 func (t *Transaction) isValid() error {
@@ -56,16 +58,22 @@ func (t *Transaction) isValid() error {
 	return nil
 }
 
-func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string) (*Transaction, error) {
+func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string, id string) (*Transaction, error) {
 	t := Transaction{
 		AccountFrom: accountFrom,
 		Amount:      amount,
 		PixKeyTo:    pixKeyTo,
+		PixKeyIdTo:  pixKeyTo.ID,
 		Status:      TransactionPending,
 		Description: description,
 	}
 
-	t.ID = uuid.NewV4().String()
+	if id == "" {
+		t.ID = uuid.NewV4().String()
+	} else {
+		t.ID = id
+	}
+
 	t.CreatedAt = time.Now()
 
 	err := t.isValid()
